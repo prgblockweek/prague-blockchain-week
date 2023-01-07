d47876f5e41db1e6ad1a2d150cb8138451c189a0
tree <tree@hidden.cz>
2023-01-07 23:14:25 +0100

    Add website

---
+++ website/svelte.config.js
@@ -0,0 +1,12 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		adapter: adapter()
+	},
+  preprocess: vitePreprocess()
+};
+
+export default config;
