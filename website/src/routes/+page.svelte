ee18f10e8629161a27468bd9d40d846c56a18100
tree <tree@hidden.cz>
2023-01-08 12:28:10 +0100

    Update

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -1,6 +1,29 @@
 <svelte:head>
-    <title>Prague Blockchain Week 2023</title>
+    <title>Prague Blockchain Week 2023 | June 2-11, 2023</title>
 </svelte:head>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-full bg-pbw-red text-pbw-white text-xl">
+    <div class="max-w-6xl mx-auto flex pt-10">
+        <div class="flex-grow"></div>
+        <div class="text-right items-end p-10">
+            <div class="uppercase">
+                <div class="text-7xl font-bold">Prague Blockchain Week</div>
+                <div class="text-5xl font-medium text-pbw-yellow">June 2-11, 2023</div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="w-full min-h-screen bg-gradient-to-b from-pbw-red to-red-900 text-pbw-white text-xl">
+    <div class="max-w-6xl mx-auto pt-7">
+        <div class="p-7">
+            <p>A decentralized conference and gathering of all people interested in cryptocurrencies and related topics. It consists of many independent events across Jun 2 - 11, including conferences, hackathons, workshops, community meetups, as well as various parties and happy hours. </p>
+            <p class="mt-5">These events will focus on a variety of different cryptocurrency ecosystems and topics such as hard money, payments, DeFi, Web3, DAOs, CryptoEconomics, NFTs, DeSci, mining, and many more</p>
+            <p class="mt-5">It is a chain agnostic and neutral community-led initiative. Our goal is not to promote one single blockchain, but rather to stimulate discussion between proponents of different solutions. The common theme is financial freedom, decentralization and privacy.</p>
+        </div>
+        <div class="flex flex-wrap gap-10 items-center text-center text-2xl uppercase mt-7 text-pbw-yellow p-7">
+            <div class="flex-grow"><a href="https://guide.prgblockweek.com" class="border border-pbw-yellow py-3 px-4 rounded hover:bg-pbw-yellow hover:text-pbw-red">Read documentation</a></div>
+            <div class="flex-grow"><a href="https://twitter.com/PrgBlockWeek" target="_blank" class="border border-pbw-yellow py-3 px-4 rounded hover:bg-pbw-yellow hover:text-pbw-red">Follow us on Twitter</a></div>
+            <div class="flex-grow"><a href="https://t.me/PrgBlockWeek" target="_blank" class="border border-pbw-yellow py-3 px-4 rounded hover:bg-pbw-yellow hover:text-pbw-red">Join Telegram group</a></div>
+        </div>
+    </div>
+</div>
