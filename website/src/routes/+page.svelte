e4527baf3dfb4220f91ecfd754082905f81a00b4
tree <tree@hidden.cz>
2023-01-15 04:02:11 +0100

    Update styles

--- website/src/routes/+page.svelte
+++ website/src/routes/+page.svelte
@@ -15,18 +15,24 @@
 </div>
 <div class="w-full min-h-screen bg-gradient-to-b from-pbw-red to-red-900 text-pbw-white text-xl">
     <div class="max-w-5xl mx-auto pt-2 md:pt-7">
-        <div class="p-7">
-            <p>A decentralized gathering of all people interested in cryptocurrencies and related topics. It consists of <a class="underline hover:no-underline hover:text-red-200" href="https://guide.prgblockweek.com/events">many independent events</a> across Jun 2 - 11, including conferences, hackathons, workshops, community meetups, as well as various parties and happy hours. </p>
+        <div class="p-7 text">
+            <p>A decentralized gathering of all people interested in cryptocurrencies and related topics. It consists of <a href="https://guide.prgblockweek.com/events">many independent events</a> across Jun 2 - 11, including conferences, hackathons, workshops, community meetups, as well as various parties and happy hours. </p>
             <p class="mt-5">These events will focus on a variety of different cryptocurrency ecosystems and topics such as hard money, payments, DeFi, Web3, DAOs, CryptoEconomics, NFTs, DeSci, mining, and many more.</p>
-            <p class="mt-5">It is a chain agnostic and neutral <a class="underline hover:no-underline" href="https://guide.prgblockweek.com/our-mission-and-team">community-led initiative</a>. Our goal is not to promote one single blockchain, but rather to stimulate discussion between proponents of different solutions. The common theme is financial freedom, decentralization and privacy.</p>
+            <p class="mt-5">It is a chain agnostic and neutral <a href="https://guide.prgblockweek.com/our-mission-and-team">community-led initiative</a>. Our goal is not to promote one single blockchain, but rather to stimulate discussion between proponents of different solutions. The common theme is financial freedom, decentralization and privacy.</p>
         </div>
         <div class="flex flex-wrap gap-10 items-center text-center text-xl md:text-2xl uppercase mt-2 md:mt-7 pb-16 md:pb-24 text-pbw-yellow p-7">
-            <div class="flex-grow"><a href="https://guide.prgblockweek.com" class="border border-pbw-yellow py-3 px-4 rounded hover:bg-pbw-yellow hover:text-pbw-red">Read documentation</a></div>
-            <div class="flex-grow"><a href="https://twitter.com/PrgBlockWeek" target="_blank" class="border border-pbw-yellow py-3 px-4 rounded hover:bg-pbw-yellow hover:text-pbw-red">Follow us on Twitter</a></div>
-            <div class="flex-grow"><a href="https://t.me/PrgBlockWeek" target="_blank" class="border border-pbw-yellow py-3 px-4 rounded hover:bg-pbw-yellow hover:text-pbw-red">Join Telegram group</a></div>
+            <div class="flex-grow"><a href="https://guide.prgblockweek.com" class="button">Read documentation</a></div>
+            <div class="flex-grow"><a href="https://twitter.com/PrgBlockWeek" target="_blank" class="button">Follow us on Twitter</a></div>
+            <div class="flex-grow"><a href="https://t.me/PrgBlockWeek" target="_blank" class="button">Join Telegram group</a></div>
         </div>
-        <div class="text-center text-red-300 pb-16 md:pb-24">
-            <a class="underline hover:no-underline hover:text-pbw-white" href="https://guide.prgblockweek.com/event-host-guide">Host your event!</a> &nbsp; ⎯ &nbsp; <a class="underline hover:no-underline hover:text-pbw-white" href="https://guide.prgblockweek.com/faq">FAQ</a> &nbsp; ⎯ &nbsp; <a class="underline hover:no-underline hover:text-pbw-white" href="https://guide.prgblockweek.com/contact-us">Contact Us</a> &nbsp; ⎯ &nbsp; <a class="underline hover:no-underline hover:text-pbw-white" href="https://guide.prgblockweek.com/impressum">Impressum</a>
+        <div id="footer" class="text-center text-red-300 pb-16 md:pb-24 mx-7 md:mx-0">
+            <a class="" href="https://guide.prgblockweek.com/event-host-guide">Host your event!</a>
+            <span class="separator">⎯</span>
+            <a class="" href="https://guide.prgblockweek.com/faq">FAQ</a>
+            <span class="separator">⎯</span>
+            <a class="" href="https://guide.prgblockweek.com/contact-us">Contact Us</a>
+            <span class="separator">⎯</span>
+            <a class="" href="https://guide.prgblockweek.com/impressum">Impressum</a>
         </div>
     </div>
 </div>
