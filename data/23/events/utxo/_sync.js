be5f71cf0ecb549d1c1db02d53a40678bcaf4568
tree <tree@hidden.cz>
2023-01-23 20:45:48 +0100

    Update photos

--- data/23/events/utxo/_sync.js
+++ data/23/events/utxo/_sync.js
@@ -1,7 +1,13 @@
 export async function data($) {
   const bundle = await $.loadJSONUrl("https://spec.utxo.cz/23/bundle.json");
   return {
-    speakers: bundle.spec.speakers,
+    speakers: bundle.spec.speakers.map(s => {
+      if (s.photos && s.photos[0]) {
+        const [ tp, ext ] = s.photos[0].split(":")
+        s.photoUrl = `https://spec.utxo.cz/22/photos/speakers/${s.id}-${tp}.${ext}`
+      }
+      return s
+    }),
     tracks: bundle.spec.tracks,
   };
 }
