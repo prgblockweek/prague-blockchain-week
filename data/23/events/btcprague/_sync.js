3e9d48170811f92408cedc4f163aa1af101b593f
tree <tree@hidden.cz>
2023-01-26 02:17:38 +0100

    BTC Prague speakers page

--- data/23/events/btcprague/_sync.js
+++ data/23/events/btcprague/_sync.js
@@ -1,16 +1,38 @@
 export async function data(tools) {
-  const $ = await tools.loadHtmlUrl("https://www.btcprague.com/");
+  const $ = await tools.loadHtmlUrl("https://www.btcprague.com/speakers");
+  //const $ = await tools.loadHtmlLocal(new URL('.', import.meta.url).pathname + "./btcprague.html");
   const out = { speakers: [] };
+
+  const peopleMapper = {
+    'Dušan Matuška': { country: 'sk' }
+  }
+
   for (const el of $(".speaker").toArray()) {
     const value = (path) => cleanup($(path, el).text());
-    out.speakers.push({
-      id: value("h3").toLowerCase().replace(/ /g, "-").trim(),
-      name: value("h3"),
+    const name = value("h3")
+    const link = $("a", el).attr("href")
+
+    const $$ = await tools.loadHtmlUrl(link)
+    const sp = $$('.container.pt-5')
+
+    const item = {
+      id: link.match(/speakers\/(.+)\/$/)[1],
+      name,
       photoUrl: $("img", el).attr("src"),
       bio: value(".popis"),
-      twitter: $(".twitter", el).attr("href")?.replace("https://twitter.com/",""),
-      web: $(".www", el).attr("href") ? { url: $(".www", el).attr("href") } : undefined,
-    });
+      twitter: $$("a.twitter", sp).attr("href")?.replace("https://twitter.com/",""),
+      linkedin: $$("a.linkedIn", sp).attr("href")?.replace("https://www.linkedin.com/in/","").replace(/\/$/,""),
+      tag: value(".taxTag "),
+      country: 'xx',
+      desc: $$('p', sp).text(),
+      web: { url: $$(".www", sp).attr("href") },
+      link
+    }
+
+    if (peopleMapper[name]) {
+      Object.assign(item, peopleMapper[name])
+    }
+    out.speakers.push(item);
   }
   return out;
 }
