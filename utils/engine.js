77977dd2670bb45d0c800b9aa83ad038c03520fb
tree <tree@hidden.cz>
2023-04-19 01:10:30 +0200

    update

--- utils/engine.js
+++ utils/engine.js
@@ -329,8 +329,7 @@ class DeConf_Collection {
           if (!sp.photoUrl) continue;
           const ext = await posix.extname(sp.photoUrl);
           const dir = [photosDir, "speakers"].join("/");
-          const ffn = (sp.id ? sp.id : nameId) + "-original" +
-            ext.replace(/\?.+$/, "");
+          const ffn = (sp.id ? sp.id : nameId) + ext.replace(/\?.+$/, "");
           const fn = [dir, ffn].join("/");
           if (await exists(fn)) {
             sp.photo = ["photos", "speakers", ffn].join("/");
