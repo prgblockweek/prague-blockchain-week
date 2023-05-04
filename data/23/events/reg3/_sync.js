3835387291efad792bea2dc33f72d051d6de6e5e
Julian Mifsud Żnuber <jmznub@skiff.com>
2023-05-04 21:12:54 +0200

    Synchronization script for Reg3

---
+++ data/23/events/reg3/_sync.js
@@ -0,0 +1,31 @@
+const peopleMapper = {
+  "Ondřej Kovařík": { country: "cz" },
+};
+
+export async function data(tools) {
+  const $ = await tools.loadHtmlUrl("https://prague.reg3.eu/speakers");
+  const out = { speakers: [] };
+
+  for (const el of $('div.w-dyn-items[role="list"] div.team-card').toArray()) {
+    const name = $("h6", el).text();
+
+    const item = {
+      id: tools.formatId(name),
+      name,
+      caption: $("p.team-member-position", el).toArray().map((x) => $(x).text())
+        .join(" ").trim(),
+      twitter: $('a[href^="https://twitter.com"]', el).attr("href")?.replace(
+        "https://twitter.com/",
+        "",
+      ),
+      photoUrl: $("img.team-card-image", el).attr("src"),
+    };
+
+    if (peopleMapper[name]) {
+      Object.assign(item, peopleMapper[name]);
+    }
+    out.speakers.push(item);
+  }
+
+  return out;
+}
