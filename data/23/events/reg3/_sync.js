18ad16fbb1c0cee659fffd828a55535c8983a622
tree <tree@hidden.cz>
2023-05-19 19:28:41 +0200

    update reg3 sync

--- data/23/events/reg3/_sync.js
+++ data/23/events/reg3/_sync.js
@@ -15,18 +15,29 @@ export async function data(tools) {
   const $ = await tools.loadHtmlUrl("https://prague.reg3.eu/speakers");
   const out = { speakers: [] };
 
-  for (const el of $('div.w-dyn-items[role="list"] div.team-card').toArray()) {
+  for (const el of $('div.team-card').toArray()) {
     const name = $("h6", el).text();
 
     const item = {
       id: tools.formatId(name),
       name,
-      caption: $("p.team-member-position", el).toArray().map((x) => $(x).text())
-        .join(" ").trim(),
+      caption: $("p.team-member-position", el).toArray().map((x, i) => {
+          let t = $(x).text().trim()
+          if (!t.match(/^at /) && i !== 0) {
+            t = (', ' + t)
+          } else {
+            t = ' ' + t
+          }
+          return t
+        }).join("").trim(),
       twitter: $('a[href^="https://twitter.com"]', el).attr("href")?.replace(
         "https://twitter.com/",
         "",
       ),
+      linkedin: $('a[href^="https://www.linkedin.com/in/"]', el).attr("href")?.replace(
+        "https://www.linkedin.com/in/",
+        "",
+      ).replace(/\/$/, ''),
       photoUrl: $("img.team-card-image", el).attr("src"),
     };
 
