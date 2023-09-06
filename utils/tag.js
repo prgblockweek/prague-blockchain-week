c2ba3fb41585374e46e23564894086010c57ace7
Afr Schoe <58883403+q9f@users.noreply.github.com>
2023-09-06 15:35:17 +0200

    update tags to calver

--- utils/tag.js
+++ utils/tag.js
@@ -1,9 +1,7 @@
 const d = new Date();
-const tag = `v${d.getFullYear()}.${("0" + (d.getMonth() + 1)).slice(-2)}.${
-  ("0" + d.getDate()).slice(-2)
-}`;
+const tag = `v${d.getFullYear() % 2000}.${(Math.ceil((d.getMonth() + 1) / 3))}`;
 const p = Deno.run({ cmd: ["git", "tag"], stdout: "piped" });
 const current = String.fromCharCode.apply(null, await p.output()).trim();
 const m = current.match(new RegExp(`${tag}`, "g"));
 const num = m ? m.length : 0;
-console.log(`TAG=${tag}-r${num}`);
+console.log(`TAG=${tag}.${num}`);
